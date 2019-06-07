676adf97f197d61419d05ca252202e8b2a04cdb7
Jones-Thomas <42827211+Jones-Thomas@users.noreply.github.com>
2019-06-07 08:10:57 -0400

    Created a script file
    User account has been created successfully

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# For emoji use this link --> https://unicode.org/emoji/charts/full-emoji-list.html#1f600
+echo -e  "\U0001f603 Starting to install the recommended utilities for ubuntu"
+
+#sudo apt update && sudo apt upgrade
+#echo "Ubuntu is up to date"
+
+# echo -e  "\U0001F600"
+# Add the new user account
+# Arguments:
+#   Account Username
+#   Account Password
+for i in $( cat /home/ubuntu/users.txt ); do
+    sudo adduser --home /home/$i  $i
+    echo "user $i added successfully!"
+    sudo echo $i:$i"123" | sudo  chpasswd
+    echo "Password for user $i changed successfully"
+    sudo usermod -aG sudo $i
+    sudo mkdir /home/$i
+    sudo chown $i:$i /home/$i
+    echo  -e "User $i \U0001F600 is ready to roll"
+done
